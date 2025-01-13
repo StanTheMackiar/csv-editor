3a1d07769bf4a8cc8f15736564a479d59dcdbe50
StanTheMackiar <callestanly@gmail.com>
2025-01-13 15:30:06 -0500

    fix(global-functions): ensure global functions are defined only in browser context

--- src/helpers/global-functions/global-functions.helper.ts
+++ src/helpers/global-functions/global-functions.helper.ts
@@ -1,37 +1,40 @@
+'use client';
 import { CustomWindow } from '@/types/window.type';
 
 declare const window: CustomWindow;
 
-window.SUM = (...args) => {
-  return String(args.reduce((acc, curr) => acc + Number(curr), 0));
-};
+if (typeof window !== 'undefined') {
+  window.SUM = (...args) => {
+    return String(args.reduce((acc, curr) => acc + Number(curr), 0));
+  };
 
-window.AVERAGE = (...args) => {
-  return String(Number(window.SUM(...args)) / args.length);
-};
+  window.AVERAGE = (...args) => {
+    return String(Number(window.SUM(...args)) / args.length);
+  };
 
-window.COUNT = (...args) => {
-  return args.length.toString();
-};
+  window.COUNT = (...args) => {
+    return args.length.toString();
+  };
 
-window.MAX = (...args) => {
-  return String(Math.max(...args.map(Number)));
-};
+  window.MAX = (...args) => {
+    return String(Math.max(...args.map(Number)));
+  };
 
-window.MIN = (...args) => {
-  return String(Math.min(...args.map(Number)));
-};
+  window.MIN = (...args) => {
+    return String(Math.min(...args.map(Number)));
+  };
 
-window.SUBTRACT = (...args) => {
-  return String(args.reduce((acc, curr) => acc - Number(curr), 0));
-};
+  window.SUBTRACT = (...args) => {
+    return String(args.reduce((acc, curr) => acc - Number(curr), 0));
+  };
 
-window.MULTIPLY = (...args) => {
-  return String(args.reduce((acc, curr) => acc * Number(curr), 1));
-};
+  window.MULTIPLY = (...args) => {
+    return String(args.reduce((acc, curr) => acc * Number(curr), 1));
+  };
 
-window.SUMA = window.SUM;
-window.PROMEDIO = window.AVERAGE;
-window.CONTAR = window.COUNT;
-window.RESTAR = window.SUBTRACT;
-window.MULTIPLICAR = window.MULTIPLY;
+  window.SUMA = window.SUM;
+  window.PROMEDIO = window.AVERAGE;
+  window.CONTAR = window.COUNT;
+  window.RESTAR = window.SUBTRACT;
+  window.MULTIPLICAR = window.MULTIPLY;
+}
