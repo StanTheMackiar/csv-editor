016c0fd8f63c849c7bc9ab5c44f196bc92d50b32
Stanly Calle <callestanly@gmail.com>
2025-01-19 22:54:38 -0500

    feat(sheet): update default row and column dimensions for improved layout

--- src/helpers/constants/sheet-config.helper.ts
+++ src/helpers/constants/sheet-config.helper.ts
@@ -1,10 +1,10 @@
 export const INITIAL_ROWS_QTY = 150;
 export const INITIAL_COLS_QTY = 20;
 
-export const ROW_DEFAULT_HEIGHT = 24;
+export const ROW_DEFAULT_HEIGHT = 28;
 export const ROW_MIN_HEIGHT = 20;
 
-export const COLUMN_DEFAULT_WIDTH = 80;
+export const COLUMN_DEFAULT_WIDTH = 120;
 export const COLUMN_MIN_WIDTH = 30;
 
 export const INITIAL_REMARKED_CELL_COORDS = { x: 0, y: 0 };
